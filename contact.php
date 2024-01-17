be474dd2c8069f834baace0fdcae883d04e0295a
karasu_corp <aquillae777@gmail.com>
2024-01-17 00:30:21 -0300

    up

--- contact.php
+++ contact.php
@@ -8,26 +8,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
 
     // Configurar o destinatário e assunto do e-mail
-    $to = "karasucyber@gmail.com"; 
+    $to = "karasucyber@gmail.com"; // Substitua pelo seu endereço de e-mail
     $subject = "Nova mensagem do formulário de contato";
 
-=    $email_body = "Nome: $name\n";
+    // Construir o corpo do e-mail
+    $email_body = "Nome: $name\n";
     $email_body .= "E-mail: $email\n";
     $email_body .= "Telefone: $phone\n\n";
     $email_body .= "Mensagem:\n$message";
 
-=    $headers = "De: $email";
+    // Configurar os cabeçalhos do e-mail
+    $headers = "De: $email";
 
-
+    // Enviar o e-mail
     $enviado = mail($to, $subject, $email_body, $headers);
 
-    
+    // Verificar se o e-mail foi enviado com sucesso
+    if ($enviado) {
         echo "success";
     } else {
         echo "error";
     }
 } else {
-    
+    // Se o método de requisição não for POST, responder com erro
     echo "error";
 }
 
