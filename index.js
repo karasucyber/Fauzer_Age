0b97dcae3e562e32fa04f650f442b4ea206d460d
karasu_corp <aquillae777@gmail.com>
2024-02-01 22:50:56 -0300

    up

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 function SendMail() {
     var params = {
-        name: document.getElementById("nome").value,
+        nome: document.getElementById("nome").value,
         email: document.getElementById("email").value,
-        phone: document.getElementById("telefone").value,
+        telefone: document.getElementById("telefone").value,
+        texto: document.getElementById("texto").value,
+
     };
 
-    const serviceID = "service_m2swido";
-    const templateID = "template_8n9ge8g";
+    const serviceID = "service_8c54czs";
+    const templateID = "template_85cdk89";
 
     emailjs.send(serviceID, templateID, params)
         .then((res) => {
             document.getElementById("nome").value = "";  // Corrected from "name" to "nome"
             document.getElementById("email").value = "";
             document.getElementById("telefone").value = "";
+            document.getElementById("texto").value = "";
             console.log(res);
             alert("Email enviado com sucesso");
         })
